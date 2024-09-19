e4ae390d49a6d8c5dbe94cba698329a599a47d54
Camillo de Jong <cydejong@icloud.com>
2024-09-19 19:25:43 +0200

    test

--- main.py
+++ main.py
@@ -63,105 +63,105 @@ def home(request: Request):
 
 # endpoints voor kasgeld functionaliteit
 
-# @app.get("/get_userdata")
-# @authentication.auth_required
-# def get_userdata(request: Request, user_id: int):
-#     return kasgeld.get_raw_userdata(user_id=user_id)
-#
-#
-# @app.get("/get_userdata_by_username")
-# @authentication.auth_required
-# def get_userdata_by_username(request: Request, username: str):
-#     return kasgeld.get_raw_userdata(username=username)
-#
-#
-# @app.post("/add_user")
-# def add_user(request: Request, userdata: AddUser, transaction_made_timestamp: float = None):
-#     return kasgeld.add_user(userdata=userdata, transaction_made_timestamp=transaction_made_timestamp)
-#
-#
-# @app.delete("/delete_user")
-# @authentication.auth_required
-# def delete_user(request: Request, user_id: int, leave_transactions: bool = False):
-#     return kasgeld.delete_user(user_id=user_id, leave_transactions=leave_transactions)
-#
-#
-# @app.put("/set_saldo")
-# @authentication.auth_required
-# def set_saldo(request: Request, user_id: int, transaction_info: TransactionField,
-#               transaction_made_timestamp: float = None):
-#     kasgeld.set_saldo(user_id=user_id, transaction_info=transaction_info,
-#                       transaction_made_timestamp=transaction_made_timestamp)
-#
-#
-# @app.get("/get_username_list")
-# @authentication.auth_required
-# def get_username_list(request: Request, use_optional_admin_login_info: bool = False):
-#     return kasgeld.get_username_list()
-#
-#
-# @app.get("/get_transaction_list")
-# @authentication.auth_required
-# def get_transaction_list(request: Request, user_id):
-#     return kasgeld.get_transaction_list(user_id=user_id)
-#
-#
-# @app.get("/get_user_exists_by_id")
-# @authentication.auth_required
-# def get_user_exists_by_id(request: Request, user_id: int):
-#     return bool(kasgeld.username_if_exists(user_id=user_id))
-#
-#
-# @app.get("/get_user_exists_by_username")
-# @authentication.auth_required
-# def get_user_exists_by_username(request: Request, username: str):
-#     return bool(kasgeld.user_id_if_exists(username=username))
-#
-#
-# @app.get("/get_username_by_id")
-# @authentication.auth_required
-# def get_username_exists_by_id(request: Request, user_id: int):
-#     return kasgeld.username_if_exists(user_id=user_id)
-#
-#
-# @app.get("/get_user_id_by_username")
-# @authentication.auth_required
-# def get_user_id_by_username(request: Request, username: str):
-#     return kasgeld.user_id_if_exists(username=username)
-#
-#
-# @app.put("/rename_user")
-# @authentication.auth_required
-# def rename_user(request: Request, user_id: int, new_username: str):
-#     return kasgeld.rename_user(user_id=user_id, new_username=new_username)
-#
-#
-# # endpoints voor admins
-# @app.post("/add_admin")
-# @authentication.auth_required
-# def add_admin(request: Request, admin_signup_info: AdminSignupField,
-#               optional_admin_login_info: None | AdminLoginField,
-#               use_optional_admin_login_info: bool = False):
-#     authentication.create_admin_account(admin_signup_info=admin_signup_info)
-#
-#
-# @app.post("/login")
-# def login(request: Request, admin_login_info: AdminLoginField):
-#     return authentication.create_session(request=request, admin_login_info=admin_login_info)
-#
-#
-# @app.get("/logout")
-# def logout(request: Request):
-#     return authentication.logout_ip(ip=request.client.host)
-#
-#
-# @app.get("/global_logout")
-# def global_logout(request: Request):
-#     admin_id = authentication.admin_id_by_session_ip(ip=request.client.host)
-#     if admin_id is None:  # was al uitgelogd:
-#         return responses.Response(status_code=status.HTTP_200_OK)
-#     # fixme: voeg informatieve return text toe maar zorg ervoor dat cookie alsnog word verwijderd
-#     return authentication.logout_id(admin_id=admin_id)
+@app.get("/get_userdata")
+@authentication.auth_required
+def get_userdata(request: Request, user_id: int):
+    return kasgeld.get_raw_userdata(user_id=user_id)
+
+
+@app.get("/get_userdata_by_username")
+@authentication.auth_required
+def get_userdata_by_username(request: Request, username: str):
+    return kasgeld.get_raw_userdata(username=username)
+
+
+@app.post("/add_user")
+def add_user(request: Request, userdata: AddUser, transaction_made_timestamp: float = None):
+    return kasgeld.add_user(userdata=userdata, transaction_made_timestamp=transaction_made_timestamp)
+
+
+@app.delete("/delete_user")
+@authentication.auth_required
+def delete_user(request: Request, user_id: int, leave_transactions: bool = False):
+    return kasgeld.delete_user(user_id=user_id, leave_transactions=leave_transactions)
+
+
+@app.put("/set_saldo")
+@authentication.auth_required
+def set_saldo(request: Request, user_id: int, transaction_info: TransactionField,
+              transaction_made_timestamp: float = None):
+    kasgeld.set_saldo(user_id=user_id, transaction_info=transaction_info,
+                      transaction_made_timestamp=transaction_made_timestamp)
+
+
+@app.get("/get_username_list")
+@authentication.auth_required
+def get_username_list(request: Request, use_optional_admin_login_info: bool = False):
+    return kasgeld.get_username_list()
+
+
+@app.get("/get_transaction_list")
+@authentication.auth_required
+def get_transaction_list(request: Request, user_id):
+    return kasgeld.get_transaction_list(user_id=user_id)
+
+
+@app.get("/get_user_exists_by_id")
+@authentication.auth_required
+def get_user_exists_by_id(request: Request, user_id: int):
+    return bool(kasgeld.username_if_exists(user_id=user_id))
+
+
+@app.get("/get_user_exists_by_username")
+@authentication.auth_required
+def get_user_exists_by_username(request: Request, username: str):
+    return bool(kasgeld.user_id_if_exists(username=username))
+
+
+@app.get("/get_username_by_id")
+@authentication.auth_required
+def get_username_exists_by_id(request: Request, user_id: int):
+    return kasgeld.username_if_exists(user_id=user_id)
+
+
+@app.get("/get_user_id_by_username")
+@authentication.auth_required
+def get_user_id_by_username(request: Request, username: str):
+    return kasgeld.user_id_if_exists(username=username)
+
+
+@app.put("/rename_user")
+@authentication.auth_required
+def rename_user(request: Request, user_id: int, new_username: str):
+    return kasgeld.rename_user(user_id=user_id, new_username=new_username)
+
+
+# endpoints voor admins
+@app.post("/add_admin")
+@authentication.auth_required
+def add_admin(request: Request, admin_signup_info: AdminSignupField,
+              optional_admin_login_info: None | AdminLoginField,
+              use_optional_admin_login_info: bool = False):
+    authentication.create_admin_account(admin_signup_info=admin_signup_info)
+
+
+@app.post("/login")
+def login(request: Request, admin_login_info: AdminLoginField):
+    return authentication.create_session(request=request, admin_login_info=admin_login_info)
+
+
+@app.get("/logout")
+def logout(request: Request):
+    return authentication.logout_ip(ip=request.client.host)
+
+
+@app.get("/global_logout")
+def global_logout(request: Request):
+    admin_id = authentication.admin_id_by_session_ip(ip=request.client.host)
+    if admin_id is None:  # was al uitgelogd:
+        return responses.Response(status_code=status.HTTP_200_OK)
+    # fixme: voeg informatieve return text toe maar zorg ervoor dat cookie alsnog word verwijderd
+    return authentication.logout_id(admin_id=admin_id)
 
 
 # just for updating code
